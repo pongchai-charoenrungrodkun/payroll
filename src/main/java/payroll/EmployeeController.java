b92a003dc1444bf87d291a3d47f9e4bd2ccc948f
Pongchai Charoenrungrodkun <53739513+pongchai-charoenrungrodkun@users.noreply.github.com>
2023-09-06 14:19:15 +0700

    Update EmployeeController.java
    Updated return response data

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -34,12 +34,10 @@ class EmployeeController {
 	CollectionModel<EntityModel<Employee>> all() {
 
 		List<EntityModel<Employee>> employees = repository.findAll().stream()
-				.map(employee -> EntityModel.of(employee,
-						linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-						linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+				.map(employee -> EntityModel.of(employee))
 				.collect(Collectors.toList());
 
-		return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+		return CollectionModel.of(employees);
 	}
 	// end::get-aggregate-root[]
 
@@ -57,9 +55,7 @@ class EmployeeController {
 		Employee employee = repository.findById(id) //
 				.orElseThrow(() -> new EmployeeNotFoundException(id));
 
-		return EntityModel.of(employee, //
-				linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-				linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+		return EntityModel.of(employee);
 	}
 	// end::get-single-item[]
 
